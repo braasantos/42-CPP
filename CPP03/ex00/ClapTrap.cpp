8afc8c327bba2dbae176baaefebe53608f1c9e8a
Braulio <brauliosantos12@outlook.com>
2024-08-06 11:54:30 +0100

    Updates

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -1,6 +1,5 @@
 #include "ClapTrap.hpp"
 
-
 int ClapTrap::get_hitPo(void)
 {
     return this->_hitPoints;
@@ -14,11 +13,11 @@ int ClapTrap::get_attDam(void)
     return this->_attDamage;
 }
 
-ClapTrap::ClapTrap(): _hitPoints(10)
+ClapTrap::ClapTrap() : _hitPoints(10), _enePoints(10), _attDamage(0)
 {
     std::cout << "Constructor was created" << std::endl;
 }
-ClapTrap::ClapTrap(std::string name) : _name(name)
+ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoints(10), _enePoints(10), _attDamage(0)
 {
     std::cout << this->_name << " was created" << std::endl;
 
@@ -27,28 +26,60 @@ ClapTrap::~ClapTrap()
 {
     std::cout << "Destructor was called" << std::endl;
 }
-// ClapTrap::ClapTrap(const ClapTrap& otherClass)
-// {
+ClapTrap::ClapTrap(const ClapTrap& otherClass)
+{
+    this->_name = otherClass._name;
+    this->_hitPoints = otherClass._hitPoints;
+    this->_enePoints = otherClass._enePoints;
+    this->_attDamage = otherClass._attDamage;
+}
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+    if (this != &other)
+    {
+        this->_name = other._name;
+        this->_hitPoints = other._hitPoints;
+        this->_enePoints = other._enePoints;
+        this->_attDamage = other._attDamage;
+        return *this;
+    }
+    return *this;
+}
 
-// }
-// ClapTrap::ClapTrap& operator=(const ClapTrap& other)
-// {
-
-// }
+int checkClapTrap(int points)
+{
+    if (points <= 0)
+        return 1;
+    return 0;
+}
 void ClapTrap::attack(const std::string& target)
 {
-    if (this->_hitPoints <= 0)
+    if (checkClapTrap(this->_hitPoints))
+    {
         std::cout << "ClapTrap is out of hit points to do anything\n";
+        return ;
+    }
     else
-        std::cout << "ClapTrap" << this->_name << "attacks" << target << ", causing" << get_attDam() << "points of damage!" << std::endl;
+        std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << get_hitPo() << " points of damage!" << std::endl;
+    this->_enePoints--;
 }
 void ClapTrap::takeDamage(unsigned int amount)
 {
-    this->_attDamage = -amount;
-    std::cout << "ClapTrap <name> attacks <target>, causing <damage> points of damage!" << std::endl;
+     if (checkClapTrap(this->_hitPoints))
+    {
+        std::cout << "ClapTrap is out of hit points to do anything\n";
+        return ;
+    }
+    this->_attDamage -=amount;
+    std::cout << "ClapTrap " << this->_name <<" was attacked causing " << this->_attDamage << " points of damage!" << std::endl;
 }
 void ClapTrap::beRepaired(unsigned int amount)
 {
-    this->_hitPoints = -amount;
-    std::cout << "ClapTrap <name> attacks <target>, causing <damage> points of damage!" << std::endl;
+     if (checkClapTrap(this->_hitPoints))
+    {
+        std::cout << "ClapTrap is out of hit points to do anything\n";
+        return ;
+    }
+    this->_hitPoints -=-amount;
+    std::cout << "ClapTrap " << this->_name << " was repaired!" << std::endl;
 }
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -3,4 +3,12 @@
 int main(void)
 {
     ClapTrap trapp("braulio");
+    ClapTrap clapp("hugo");
+    trapp.attack("hugo");
+    trapp.takeDamage(10);
+    trapp.beRepaired(1);
+    clapp.attack("braulio");
+    clapp.takeDamage(10);
+    clapp.beRepaired(1);
+    return 0;
 }
